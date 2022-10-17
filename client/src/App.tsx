e16d42a359dec11eb49cb2ec1e33426d6c2b0cba
Vencyev <steammingliu1@gmail.com>
2022-10-17 15:45:25 -0800

    route change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,10 +10,10 @@ const App:FC = (): JSX.Element => {
   return (
     <>
     <Routes>
-    <Route path="/" element={<Navbar />} >
-      {/* <Route index element={Home}/> */}
-    </Route>
-    {/* <Route path="*" element={<PageNotFound/>} /> */}
+      <Route path="/" element={<Navbar />} >
+        {/* <Route index element={Home}/> */}
+      </Route>
+      {/* <Route path="*" element={<PageNotFound/>} /> */}
     </Routes>
       <Navbar />
       <div className="grid items-center w-full h-[calc(100%-56px)] bg-primary-black text-white">
