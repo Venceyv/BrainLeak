2414d396bfd6f263cec59097e8c053d8df5ade36
Vencyev <steammingliu1@gmail.com>
2022-10-17 13:22:07 -0800

    ...

--- server/routes/tools.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {getFileUrl} from '../controllers/tools.js';
-import { upload } from '../configs/googleCloud.js';
-const toolsRouter = express();
-
-toolsRouter.post('/uploadImg',upload.single('imgFile'),getFileUrl);
-toolsRouter.post('/uploadVideo',upload.single('videoFile'),getFileUrl);
-export {toolsRouter};
