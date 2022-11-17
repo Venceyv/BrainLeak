cc3b6f30b3ba2cab755dac731f70fe15079ccb16
Anmol Singh <mahal_anmol@yahoo.com>
2022-11-17 00:26:01 -0800

    Add tag to post, need more work still

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useMemo, useState } from "react";
 
 let profile = {
   name: "HelloKitty",
@@ -12,23 +12,38 @@ let list = [
   {
     user: "byeKitty",
     likes: 343,
+    subscribers: 34,
   },
   {
-    user: "nexon",
+    user: "squidward",
     likes: 23,
+    subscribers: 93,
   },
   {
     user: "mapleStory",
     likes: 343,
+    subscribers: 56,
   },
 ];
 
 const CreatePost: React.FC = () => {
+  let [tag, setTag] = useState<string | null>(null);
+  let [addTag, setaddTag] = useState<any>([]);
+
+  let changeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    setTag(e.target.value);
+  };
+  let addTags = () => {
+    const newTag = tag;
+    setaddTag([...addTag, newTag]);
+  };
+
   return (
     <>
       <div className="bg-secondary-black w-full flex justify-center  h-screen pt-4">
         {/* form */}
-        <div className="w-2/6 h-5/6 border bg-primary-black border-white ">
+        <div className="w-5/12 h-5/6 border bg-primary-black border-white  ">
           {" "}
           <h2 className="text-white">Create a Post</h2>
           <div className="flex justify-center">
@@ -51,7 +66,18 @@ const CreatePost: React.FC = () => {
                 <div className="flex justify-between">
                   <div className="flex">
                     <p className="text-white text-md">Add tags: </p>
-                    <button className="text-white text-sm border border-white w-12 rounded ml-2">
+                    <input
+                      type="text"
+                      placeholder="Tag..."
+                      name="tag"
+                      size={10}
+                      onChange={changeHandler}
+                    ></input>
+                    <button
+                      type="button"
+                      className="text-white text-sm border border-white w-12 rounded ml-2"
+                      onClick={addTags}
+                    >
                       +
                     </button>
                   </div>
@@ -73,54 +99,85 @@ const CreatePost: React.FC = () => {
                   </div>
                 </div>
                 <div className="flex mt-4">
-                  <button className="text-white text-sm border border-white w-12 rounded ">
-                    Game
-                  </button>
-                  <button className="text-white text-sm border border-white w-12 rounded ml-4">
-                    Funny
-                  </button>
-                  <button className="text-white text-sm border border-white w-12 rounded ml-4">
-                    Meme
-                  </button>
+                  {addTag.map((item: any, key: number) => {
+                    return (
+                      <>
+                        <div
+                          key={key}
+                          className="text-white text-sm border border-white w-12 rounded ml-4 text-center"
+                        >
+                          {item}
+                        </div>
+                      </>
+                    );
+                  })}
                 </div>
               </form>
             </div>
           </div>
         </div>
         {/* you profile and other users  */}
-        <div
-          id="outer"
-          className="  bg-primary-black w-1/6 h-5/6  flex  justify-center"
-        >
-          <div className="border border-white h-1/3 rounded-lg w-3/4 ">
-            <div className="flex justify-center h-2/4">
-              <div className=" border border-white flex flex-wrap mt-2 w-11/12 h-7/8 rounded-lg">
-                <img
-                  className="w-4 h-4 rounded-full border-[1px] cursor-pointer border-white mt-2 ml-2"
-                  src="/src/assets/img/testUserPic.jpeg"
-                  alt="user"
-                />
-                <p className="text-white text-sm mt-2 ml-2 ">{profile.name}</p>
-                <p className="basis-full text-xs text-white text-center mb-px">
-                  Subscribers:{profile.subscribers}
-                </p>
+        <div id="outer" className="  bg-primary-black w-1/6 h-5/6  ">
+          <div className="flex  justify-center">
+            <div className="border border-white h-1/3 rounded-xl w-3/4 ">
+              <div className="flex justify-center h-2/4">
+                <div className=" border border-white flex flex-wrap mt-2 w-11/12 h-7/8 rounded-xl">
+                  <img
+                    className="w-4 h-4 rounded-full border-[1px] cursor-pointer border-white mt-2 ml-2"
+                    src="/src/assets/img/testUserPic.jpeg"
+                    alt="user"
+                  />
+                  <p className="text-white text-sm mt-2 ml-2 ">
+                    {profile.name}
+                  </p>
+                  <p className="basis-full text-xs text-white text-center mb-px">
+                    Subscribers:{profile.subscribers}
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="flex justify-center mt-2">
-              <div>
-                <p className="text-xs text-white ">
-                  <span>&#9830;</span>Post Count: {profile.posts}
-                </p>
-                <p className="text-xs text-white">
-                  <span>&#9830;</span>Comment Count: {profile.comments}
-                </p>
-                <p className="text-xs text-white">
-                  <span>&#9830;</span>Like Count: {profile.likes}
-                </p>
+              <div className="flex justify-center mt-2">
+                <div>
+                  <p className="text-xs text-white ">
+                    <span>&#9830;</span>Post Count: {profile.posts}
+                  </p>
+                  <p className="text-xs text-white">
+                    <span>&#9830;</span>Comment Count: {profile.comments}
+                  </p>
+                  <p className="text-xs text-white">
+                    <span>&#9830;</span>Like Count: {profile.likes}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
           {/* People similar to you */}
+          <div className="flex justify-center ">
+            <div className="w-8/12">
+              {list.map((item, key) => {
+                return (
+                  <>
+                    <div className="border border-white text-white text-xs flex flex-wrap h-1/3 mt-6 rounded-2xl">
+                      <img
+                        className="w-4 h-4 rounded-full border-[1px] cursor-pointer border-white mt-2 ml-2"
+                        src="/src/assets/img/testUserPic.jpeg"
+                        alt="user"
+                        key={key}
+                      />
+                      <p className="text-white text-sm mt-2 ml-2">
+                        {item.user}
+                      </p>
+                      <p className="basis-full text-xs text-white text-center mb-px">
+                        <p className="basis-full text-xs text-white text-center mb-px">
+                          Likes: {item.likes}
+                        </p>
+                        Subscribers: {item.subscribers}
+                      </p>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </>
