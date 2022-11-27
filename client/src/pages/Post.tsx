b693111ff2f26ae549f252c96ff7a8d71df4bcb0
Ming Lou <steammingliu1@gmail.com>
2022-11-27 03:36:10 -0800

    Post overlay fixed

--- client/src/pages/Post.tsx
+++ client/src/pages/Post.tsx
@@ -6,13 +6,15 @@ import { NewComment } from '../feature/UserPost/components/NewComment';
 
 export const Post: FC = (): JSX.Element => {
   return (
-    <div className="absolute top-0 w-full max-w-[1024px] z-10 bg-post-bg-black">
-      <div className="flex flex-col justify-center w-full h-[calc(100%-56px)]">
-        <div className="relative flex flex-row w-full max-w-[1024px] h-full ">
-          <UserPost />
-          <PostAuthor />
+    <div className="absolute flex items-center justify-center top-0 w-full z-10 bg-primary-black">
+      <div className="w-full max-w-[1024px] z-10 bg-post-bg-black">
+        <div className="flex flex-col justify-center w-full h-[calc(100%-56px)]">
+          <div className="relative flex flex-row w-full max-w-[1024px] h-full ">
+            <UserPost />
+            <PostAuthor />
+          </div>
+          <Comment />
         </div>
-        <Comment />
       </div>
     </div>
   );
