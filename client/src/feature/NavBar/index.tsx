c91f48798849541836dce20cfda5e668f301bbb6
Ming Lou <steammingliu1@gmail.com>
2022-11-27 19:20:17 -0800

    close post; logo nav

--- client/src/feature/NavBar/index.tsx
+++ client/src/feature/NavBar/index.tsx
@@ -4,15 +4,19 @@ import { SearchItem } from './components/SearchItem';
 import { NotificationItem } from './components/NotificationItem';
 import { LoginItem } from './components/LoginItem';
 import { useCheckAuth } from './index.hook';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export const NavBar: FC = (): JSX.Element => {
   const [isLoggedIn, setLogin] = useState<boolean>(false);
   useCheckAuth({ setLogin });
+  const navigate = useNavigate();
 
   return (
     <div className="sticky flex items-center justify-start top-0 flex-1 h-[56px] w-full gap-8 px-4 mx-auto drop-shadow-md z-50 bg-secondary-black">
-      <div className="flex items-center text-white ">
+      <div
+        className="flex items-center cursor-pointer text-white "
+        onClick={() => navigate('')}
+      >
         <span className="sr-only">Home</span>
         <div className="h-full font-gemini text-3xl m-2">BrainLeak</div>
       </div>
--- client/src/feature/UserPost/components/TopBar.tsx
+++ client/src/feature/UserPost/components/TopBar.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { formatNumber } from '../../../utils/formatNumber';
 
 export const TopBar: FC<{ likes: number; dislikes: number }> = ({
   likes,
   dislikes,
 }): JSX.Element => {
+  const navigate = useNavigate();
   return (
     <div className="absolute flex gap-3 mb-2 p-[2px] w-[calc(100%-32px)] border-2 rounded-md bg-secondary-black border-border-black">
       <div className="flex gap-3 pl-1">
@@ -34,6 +36,7 @@ export const TopBar: FC<{ likes: number; dislikes: number }> = ({
         src="../../../assets/img/close-hanger.svg"
         className="w-8 h-8 cursor-pointer ml-auto mr-5"
         alt="close"
+        onClick={() => navigate(-1)}
       />
       {/* <img src="../../../assets/img/close.svg" className="w-6 h-6 cursor-pointer" alt="like" /> */}
     </div>
