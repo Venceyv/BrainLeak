d925acc8cb10a111a1e87506f239240a638067ea
Vencyev <steammingliu1@gmail.com>
2022-11-28 16:08:00 -0800

    comment input validation

---
+++ client/src/feature/Search/index.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const SearchResult: FC = (): JSX.Element => {
+  return <div></div>;
+};
---
+++ client/src/pages/Search.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+export const Post: FC = (): JSX.Element => {
+  return <div></div>;
+};
+
+// TODO: POST VALIDATOR
