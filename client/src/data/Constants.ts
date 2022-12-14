3ef1aaa84a960d038c947ca7b7762881a687e320
Ming Lou <steammingliu1@gmail.com>
2022-12-14 00:02:14 -0800

    ...

--- client/src/data/Constants.ts
+++ client/src/data/Constants.ts
@@ -1,5 +1,4 @@
-// const URL: string = 'https://www.vydnb.com';
-const URL: string = 'http://localhost:3000';
+const URL: string = 'https://www.vydnb.com';
 const USER_PATH: string = `${URL}/users`;
 const CLIENT_ID: string = `324694170288-njdv09i6kjhpoicc03c8tonqnrd914ps.apps.googleusercontent.com`;
 
