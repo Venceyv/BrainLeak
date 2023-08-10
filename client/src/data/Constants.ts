73c1ca5efe8e019b7775853e9b1ac57b351ea227
Ming Lou <steammingliu1@gmail.com>
2023-08-10 00:45:41 -0700

    [change]: env conf

--- client/src/data/Constants.ts
+++ client/src/data/Constants.ts
@@ -1,6 +1,6 @@
-const URL: string = 'https://www.vydnb.com';
+const URL: string = process.env.URL ?? '';
 // const URL: string = 'http://localhost:3000';
-const USER_PATH: string = `${URL}/users`;
-const CLIENT_ID: string = `324694170288-njdv09i6kjhpoicc03c8tonqnrd914ps.apps.googleusercontent.com`;
+const USER_PATH: string = process.env.USER_PATH ?? '';
+const CLIENT_ID: string = process.env.CLIENT_ID ?? '';
 
 export { URL, USER_PATH, CLIENT_ID };
